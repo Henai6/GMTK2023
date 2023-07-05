d629a44b20dc745abac2eb5442c9b843a360cadd
Mat Hentia <mathiasarbejdsmail@gmail.com>
2023-07-05 18:18:39 +0200

    Changed to use built in input manager

--- Assets/Code/Input/NativeInputReader.cs
+++ Assets/Code/Input/NativeInputReader.cs
@@ -15,12 +15,11 @@ namespace CustomInput
         private void Update()
         {
             _v = Vector2.zero;
-            if (Input.GetKey(KeyCode.W)) { _v.y += 1; }
-            if (Input.GetKey(KeyCode.S)) { _v.y -= 1; }
-            if (Input.GetKey(KeyCode.D)) { _v.x += 1; }
-            if (Input.GetKey(KeyCode.A)) { _v.x -= 1; }
+            _v.y += Input.GetAxisRaw("Vertical");
+            _v.x += Input.GetAxisRaw("Horizontal");
             movement = _v.normalized;
-            isJumping = Input.GetKeyDown(KeyCode.Space);
+
+            isJumping = Input.GetButtonDown("Jump");
             isSprinting = Input.GetKey(KeyCode.LeftShift);
         }
     }
