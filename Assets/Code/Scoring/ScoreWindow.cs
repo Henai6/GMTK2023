79cd9dec5e22d7ef5e87fd50b03022510af5bc68
AweiTAS <811426275@qq.com>
2023-07-08 12:00:12 +0100

    add scoring

---
+++ Assets/Code/Scoring/ScoreWindow.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreWindow : MonoBehaviour
+{
+    private TextMeshProUGUI text;
+    void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Update()
+    {
+        if (text != null) { Debug.Log("exist"); text.text = ScoringManager.Instance.GetScore().ToString(); }
+        else text = GetComponent<TextMeshProUGUI>();
+    }
+}
---
+++ Assets/Code/Tetris Test/ScoreTest.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreTest : MonoBehaviour
+{
+    public void AddOne()
+    {
+        ScoringManager.Instance.AddScore(1);
+    }
+
+    public void AddTen()
+    {
+        ScoringManager.Instance.AddScore(10);
+    }
+    public void Clear()
+    {
+        ScoringManager.Instance.ClearScore();
+    }
+}
