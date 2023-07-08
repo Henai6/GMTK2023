015a6b832765b5dbbd698ca3422bd99da6e0c76f
Mat Hentia <mathiasarbejdsmail@gmail.com>
2023-07-08 16:24:11 +0200

    slight improvement to movement and otehr code

--- Assets/Code/Tetris/Bucket.cs
+++ Assets/Code/Tetris/Bucket.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,12 +8,13 @@ public enum gameDirection { Up, Left, Right, Down }
 
 public class Bucket : MonoBehaviour
 {
-    public static Bucket player;
+    public static Bucket player; //I have not bothered ensuring it is a singleton
 
     private gameDirection dir = gameDirection.Up;
     private Position pivot = new Position(0, 0);
+    private Position pos = new Position(0, 0);
 
-    public Tile[,] grid = new Tile[10, 20];
+    public Tile[,] grid = new Tile[9, 19];
 
     private float tickSpeed = 0.5f;
     private float curTime = 0f;
@@ -30,23 +33,76 @@ public class Bucket : MonoBehaviour
 
     void Update()
     {
-        curTime += Time.deltaTime;
-        if (curTime > tickSpeed)
+        if (Input.GetButtonDown("Horizontal"))
         {
-            curTime = 0f;
-            float horizontalInput = Input.GetAxisRaw("Horizontal");
-            float verticalInput = Input.GetAxisRaw("Vertical");
-
-            Vector3 bucketPos = this.transform.position;
-            if (horizontalInput != 0)
-            {
-                bucketPos.x += OuterGrid.tileScale * horizontalInput;
-            }
-            if (verticalInput != 0)
-            {
-                bucketPos.y += OuterGrid.tileScale * verticalInput;
-            }
-            this.transform.position = bucketPos;
+            pos.x += (int)Input.GetAxisRaw("Horizontal");
         }
+        if (Input.GetButtonDown("Vertical"))
+        {
+            pos.y += (int)Input.GetAxisRaw("Vertical");
+        }
+        this.transform.position = OuterGrid.TranslateToGridCoordinates(pos);
+    }
+
+    public Position[] GetGrid()
+    {
+        List<Position> tileList = new List<Position>();
+
+        int yDir = 1;
+        int xDir = 1;
+        if (dir == gameDirection.Down) { yDir = -1; }
+        if (dir == gameDirection.Left) { xDir = -1; }
+
+        for (int i = 0; i > grid.GetLength(0); i++)
+        {
+            for (int j = 0; j > grid.GetLength(1); j++)
+            {
+                if (grid[i,j] != null)
+                {
+                    tileList.Add(new Position(pos.x + i * xDir, pos.y + j * yDir));
+                    //^^ maybe not ideal, as each tile in the bucket causes a object to be created every tick
+                }
+            }
+        }
+        return null;
+    }
+
+    public bool HitBottomLine(Position other, gameDirection otherDir)
+    {
+        int width = grid.GetLength(0);
+        Position[] bottomLine = new Position[width];
+
+        switch (dir, otherDir)
+        {
+            //Not sure if bottomLine is correct with the -1
+            case (gameDirection.Up, gameDirection.Down):
+                for (int i = 0; i < width; i++)
+                {
+                    bottomLine[i] = new Position(pos.x - (width - 1)/2 + i, pos.y - 1);
+                }
+                break;
+            case (gameDirection.Down, gameDirection.Up):
+                for (int i = 0; i < width; i++)
+                {
+                    bottomLine[i] = new Position(pos.x - (width - 1) / 2 + i, pos.y);
+                }
+                break;
+            case (gameDirection.Left, gameDirection.Right):
+                for (int i = 0; i < width; i++)
+                {
+                    bottomLine[i] = new Position(pos.x - 1, pos.y - (width - 1) / 2 + i);
+                }
+                break;
+            case (gameDirection.Right, gameDirection.Left):
+                for (int i = 0; i < width; i++)
+                {
+                    bottomLine[i] = new Position(pos.x, pos.y - (width - 1) / 2 + i);
+                }
+                break;
+            default:
+                return false;
+        }
+
+        return bottomLine.Any(p => p.x == other.x && p.y == other.y);
     }
 }
